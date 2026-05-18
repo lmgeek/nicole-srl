f236576ce11c7742c44ac7f4d8e943fb7648c58e
Luis Marin <tsuluismarin@gmail.com>
2026-05-18 18:18:21 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const API_URL = import.meta.env.VITE_API_URL || 'http://148.230.71.90:3001';
 
 class ApiService {
   getToken() {
